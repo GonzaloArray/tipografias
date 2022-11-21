4daa70ba60555b8a75000049060799823402a7e1
GonzaloArray <gonzalo.arrayaran@gmail.com>
2022-11-20 23:40:24 -0300

    Agregando fuente estatica

--- js/favoritos.js
+++ js/favoritos.js
@@ -12,6 +12,18 @@ function inciarApp() {
 }
 
 function mostrarFav(fav) {
+
+    if (fav.length == 0) {
+        const parrafo = document.createElement('div');
+        parrafo.className = "bg-dark py-4 text-center fw-bold text-light rounded"
+        parrafo.innerHTML = /* html */`
+            <p>No agrego ninguna tipografia aún, vaya por una pequeño tipografo</p>
+            <a href="../html/catalogo.html" class="nav-link btn btn-success d-inline py-2 px-3 my-2">Ir a catalogo...</a>
+        `;
+
+        toShowCard.appendChild(parrafo);
+    }
+
     fav.forEach((element) => {
 
         const { nombre, origen, varibales, url_local, id } = element;
